675eb6ef5bdb6c63a662d02edf6286c7ad98a6a8
Abinash Sethi <148425282+MrRobotTheCoder@users.noreply.github.com>
2025-05-13 11:26:19 -0400

    commented out extensions which were not working.

--- RHEL_8_&_9/vs_code_rhel_devops.sh
+++ RHEL_8_&_9/vs_code_rhel_devops.sh
@@ -17,20 +17,20 @@ code --install-extension redhat.vscode-yaml
 code --install-extension ms-vscode-remote.remote-ssh
 code --install-extension hashicorp.terraform
 code --install-extension jenkinsxio.vscode-jx-tools
-code --install-extension cschleiden.vscode-github-actions
+# code --install-extension cschleiden.vscode-github-actions
 code --install-extension redhat.ansible
 code --install-extension amazonwebservices.aws-toolkit-vscode
 code --install-extension googlecloudtools.cloudcode
-code --install-extension julianhr.prometheus
+# code --install-extension julianhr.prometheus
 code --install-extension grafana.grafana-vscode
 code --install-extension ms-python.python
 code --install-extension timonwong.shellcheck
-code --install-extension aquasecurity.vscode-trivy
-code --install-extension timoschwarzer.vscode-helm
+# code --install-extension aquasecurity.vscode-trivy
+# code --install-extension timoschwarzer.vscode-helm
 code --install-extension humao.rest-client
 
 # Final comment:
 echo ""
-echo " You are ready to use VS Code, loaded with all the required plugins!! "
+echo "You are ready to use VS Code, loaded with all the required plugins!! "
 echo ""
 echo "Happy Learning!!"
